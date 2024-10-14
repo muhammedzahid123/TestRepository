a5a6a63fabb2a1f25f82b01f8d1ad97e25e2a7c2
muhammedzahid123 <zaaahid2109@gmail.com>
2024-10-14 14:33:22 +0400

    Add files via upload

---
+++ Homework 1.py
@@ -0,0 +1,22 @@
+print("Hello, World!")
+print(5+3)
+print("python","is","fun!")
+print("She said, \"Hello!\"")
+print("First line\nSecond line")
+print("Item 1\tItem 2\tItem 3")
+print("The result is", 42)
+print("The result is", 42)
+print("The result is", 42)
+print("Dollar sign: $")
+print("apple", "banana", "cherry", sep=", ")
+print("Hello,", end=" ")
+print("Python! " * 3)
+print("\u2764") # Prints a heart symbol
+print()
+print("Hello " + "World!")
+print(True)
+print(False)
+print(1.2e3) # Prints 1200.0
+print("""This is a multiline
+    print statement.""")
+print("This is a backslash: \\")
